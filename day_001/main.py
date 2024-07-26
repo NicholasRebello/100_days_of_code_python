ad502df42b9600708e7797f022c3fc285c4cca2c
Nicholas Rebello <nicholas@rebello.dev>
2024-07-26 14:48:23 -0700

    pushing day001 project

---
+++ day_001/main.py
@@ -0,0 +1,5 @@
+print("Welcome to the Band Name Generator program!")
+city_name = input("What city did you grow up in?\n")
+pet_name = input("What is the name of a pet?\n")
+
+print("Your band name is: " + city_name + " " + pet_name)
